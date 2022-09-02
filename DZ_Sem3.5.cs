47a5377fdb405f33ecc9155d6ffa4815d39cb852
Rick Solo <mrfox7398@mail.ru>
2022-09-02 20:26:37 +0300

    Zadacha 2

--- DZ_Sem3.5.cs
+++ DZ_Sem3.5.cs
@@ -1,19 +1,24 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+// ______
 
-Console.Write("Введите количество элементов: ");
-int size = Convert.ToInt32(Console.ReadLine());
-int [] array = new int [size];
-int count = 0;
+// Console.Write("Введите количество элементов: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int [] array = new int [size];
+// int count = 0;
 
-for (int i=0; i<size; i++)
-{
-    array[i]= new Random().Next (100,1000);
-    count++;
-}
-Console.Write("[{0}]",string.Join(", ",array));
-Console.Write(" -> "+ count);
+// for (int i=0; i<size; i++)
+// {
+//     array[i]= new Random().Next (100,1000);
+//     if (array[i]%2 == 0)
+//     {
+//         count++;
+//     }
+    
+// }
+// Console.Write("[{0}]",string.Join(", ",array));
+// Console.Write(" -> "+ count);
 
 
 //--------------------------------------------------------------------------
@@ -22,7 +27,25 @@ Console.Write(" -> "+ count);
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+// _________
 
+
+Console.Write("Введите количество элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [size];
+int sum = 0;
+
+for (int i=0; i<size; i++)
+{
+    array[i]= new Random().Next ();
+    if (i%2 != 0)
+    {
+        sum+=array[i];
+    }
+    
+}
+Console.Write("[{0}]",string.Join(", ",array));
+Console.Write(" -> "+ sum);
 
 //--------------------------------------------------------------------------------
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
